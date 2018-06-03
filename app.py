75b836ef729f0ba5d5471ab85dcb1366557e815d
Jason Yang <jy_jason98@hotmail.com>
2018-06-03 19:00:26 -0400

    action cateogries - get all game titles by sub cateogry

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+from models import GameCategories
+import time
 
 from selenium import webdriver 
 from selenium.webdriver.common.by import By 
@@ -8,17 +10,40 @@ from selenium.common.exceptions import TimeoutException
 
 app = Flask(__name__)
 
+action_url = "https://play.google.com/store/apps/category/GAME_ACTION?hl=en"
 
 browser = webdriver.Chrome();
-browser.get("https://play.google.com/store/apps/category/GAME/collection/topselling_free?hl=en");
+browser.get("https://play.google.com/store/apps/category/GAME_ACTION?hl=en");
 
-game_titles = browser.find_elements_by_xpath('//a[@class="title"]')
+#game categories objects of all game titles and category
+all_game_categories = []
 
-num_pages_titles = len(game_titles)
+action_categories = browser.find_elements_by_xpath('//h2[@class="single-title-link"]')
+for category in action_categories:
+	category_titles = category.find_elements_by_xpath('//a[@class="title-link id-track-click"]')
+	print(category_titles[0].text)
+	category_buttons = category.find_elements_by_xpath('//a[@class="see-more play-button small id-track-click apps id-responsive-see-more"]')
+	category_buttons[0].click()
+	game_title_elements = browser.find_elements_by_xpath('//a[@class="title"]')
+
+	game_titles = []
+	for elem in game_title_elements:
+		print(elem.text)
+		game_titles.append(elem.text)
+
+	game_category = Category(category_title, game_titles)
+	all_game_categories.append(game_category)
+
+	delay = 1 # wait before doing the next scrae on another category
+	time.sleep(10)
+
+	driver.execute_script("window.history.go(-1)")
+
+
 
 @app.route('/')
 def index():
-	return render_template('home.html', game_titles = game_titles)
+	return render_template('home.html', all_game_categories = all_game_categories)
 
 if __name__ == '__main__':
 	app.run(debug=True)
