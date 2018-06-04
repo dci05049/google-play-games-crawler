d837f67739df706c517a1a88c8efdc7758b034e6
Jason Yang <jy_jason98@hotmail.com>
2018-06-04 13:34:18 -0400

    models folder

---
+++ models/GameCategories.py
@@ -0,0 +1,8 @@
+class Cateogry:
+	category_title = "Not Found"
+	all_titles = []
+
+	def __init__(self, category_title, all_titles):
+		self.category_title = category_title
+		self.all_titles = all_titles
+
