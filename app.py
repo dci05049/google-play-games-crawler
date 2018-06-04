6d828d390bd47ca87f649f02f74ff01998b4fda7
woongbinchoi <woongbinchoi@gmail.com>
2018-06-04 17:14:13 -0400

    made dictionary for games

--- app.py
+++ app.py
@@ -1,40 +1,47 @@
 from flask import Flask, render_template
 from models import GameCategories
 import time
+import collections
 
 from selenium import webdriver 
 from selenium.webdriver.common.by import By 
 from selenium.webdriver.support.ui import WebDriverWait 
-from selenium.webdriver.support import expected_conditions as EC 
-from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import TimeoutException, StaleElementReferenceException
+
+def WaitforElement(browser, timeout, xpathElement):
+	try:
+	    WebDriverWait(browser, timeout).until(EC.visibility_of_element_located((By.XPATH, xpathElement)))
+	except TimeoutException:
+	    print("Timed out waiting for page to load")
+	    browser.quit()
 
 app = Flask(__name__)
 
 action_url = "https://play.google.com/store/apps/category/GAME_ACTION?hl=en"
 
-browser = webdriver.Chrome();
-browser.get(action_url);
+browser = webdriver.Chrome()
+browser.get(action_url)
 
 #game categories objects of all game titles and category
-all_game_categories = []
-increment = 0;
+all_game_categories = collections.OrderedDict()
 
-action_categories = browser.find_elements_by_xpath('//a[contains(text(), "See more")]') # get all the see more buttons
-for category in action_categories:
-	category_titles = category.find_elements_by_xpath('//a[@class="title-link id-track-click"]') # get the category title
-	category.click() # click see more button
-	time.sleep(1) #might need to fix
+title_categoryElements = browser.find_elements_by_xpath('//a[@class="title-link id-track-click"]') # get all the Title buttons
+title_categories = [(x.text, x.get_attribute('href')) for x in title_categoryElements]
+for title in title_categories:
+	browser.get(title[1])
 
 	game_title_elements = browser.find_elements_by_xpath('//a[@class="title"]')
+	gameTitles = [elem.text.encode("utf-8", "ignore") for elem in game_title_elements]
 
-	game_titles = []
-	for elem in game_title_elements:
-		print(elem.text)
-		game_titles.append(elem.text)
+	all_game_categories[title[0]] = gameTitles
 
-	browser.execute_script("window.history.go(-1)")
-	time.sleep(1)
-
+for category, gamelist in all_game_categories.items():
+	print(category)
+	print(gamelist)
+	print('\n')
 
 
 @app.route('/')
